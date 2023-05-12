d571f921980446f1c90366720fda6e84a70fb518
moonishkale <manishkhadgi123@gmail.com>
2023-05-12 17:46:39 +1000

    server update

--- server.js
+++ server.js
@@ -2,3 +2,22 @@ import express from "express";
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+import cors from "cors";
+import morgan from "morgan";
+//uncaught routers
+app.use("*", (req, res, next) => {
+  const error = {
+    errorCode: 404,
+    message: "Requested resources not found",
+  };
+  next(error);
+});
+
+//middlwares
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`server running at http://localhost:${PORT}`);
+});
